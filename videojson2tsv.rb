0220f29fa189f5778b4c01b1276b1bf9d0ce9547
Ayahiko Niimi <a-niimi@qc4.so-net.ne.jp>
2013-08-07 14:44:52 +0900

    Create videojson2tsv.rb

---
+++ videojson2tsv.rb
@@ -0,0 +1,31 @@
+#!/bin/env ruby
+
+require 'json'
+require 'zlib'
+require 'nkf'
+opt = "-w -h -Z1"
+
+video_dir = "."
+tag_dir = "."
+
+ARGV.each{|filename|
+  Zlib::GzipReader.open(filename) do |gz|
+    video_file = Zlib::GzipWriter.open(video_dir + "/" + File.basename(filename, ".dat.gz") + "video_sampling100.tsv.gz")
+    tag_file = Zlib::GzipWriter.open(tag_dir + "/" + File.basename(filename, ".dat.gz") + "tags_sampling100.tsv.gz")
+    while line = gz.gets
+
+      line = NKF.nkf(opt, line)
+      line.gsub!(/\t/, "\\t")
+      row = JSON.parse(line)
+
+      next unless /00$/ === row["video_id"]
+      video_file.puts [ row["video_id"], row["thread_id"], row["title"], row["description"], row["thumbnail_url"], row["upload_time"], row["length"], row["movie_type"], row["size_high"], row["size_low"], row["view_counter"], row["comment_counter"], row["mylist_counter"], row["last_res_body"] ].join("\t")
+
+      row["tags"].each{|tag|
+        tag_file.puts [ row["video_id"], tag["tag"], tag["category"], tag["lock"] ].join("\t")
+      }
+    end
+  video_file.close
+  tag_file.close
+  end
+}

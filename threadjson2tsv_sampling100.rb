298ac6e348f54a73f5351c3a963441ae41ee0d6f
Ayahiko Niimi <a-niimi@qc4.so-net.ne.jp>
2013-11-08 12:06:10 +0900

    Create threadjson2tsv_sampling100.rb

---
+++ threadjson2tsv_sampling100.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'zlib'
+require 'nkf'
+opt = "-w"
+
+thread_dir = "."
+
+ARGV.each{|filename|
+  File.open(filename) do |dat|
+    thread_file = Zlib::GzipWriter.open(thread_dir + "/" + File.basename(filename, ".dat") + "thread.tsv.gz")
+    video_id = File.basename(filename, ".dat")
+    next unless /00$/ === row["video_id"]
+    while line = dat.gets
+      
+      line = NKF.nkf(opt, line)
+      line.gsub!(/\t/, "\\t")
+      row = JSON.parse(line)
+      
+      thread_file.puts [ video_id, row["date"], row["no"], row["vpos"], row["comment"], row["command"] ].join("\t")
+      
+    end
+    thread_file.close
+  end
+}

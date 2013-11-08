110a8749d17b34d335341f5ccf6c0f4491d1ce25
Ayahiko Niimi <a-niimi@qc4.so-net.ne.jp>
2013-11-09 06:22:27 +0900

    Update threadjson2tsv_sampling100.rb

--- threadjson2tsv_sampling100.rb
+++ threadjson2tsv_sampling100.rb
@@ -2,25 +2,26 @@
 
 require 'json'
 require 'zlib'
-require 'nkf'
-opt = "-w"
 
 thread_dir = "."
 
 ARGV.each{|filename|
   File.open(filename) do |dat|
-    thread_file = Zlib::GzipWriter.open(thread_dir + "/" + File.basename(filename, ".dat") + "thread.tsv.gz")
     video_id = File.basename(filename, ".dat")
     next unless /00$/ === row["video_id"]
+
+    thread_file = Zlib::GzipWriter.open(thread_dir + "/" + File.basename(filename, ".dat") + "thread.tsv.gz")
     while line = dat.gets
       
-      line = NKF.nkf(opt, line)
       row = JSON.parse(line)
       comment = row["comment"]
       comment.gsub!(/\t/, "\\t")
       comment.gsub!(/\n/, "\\n")
-      
-      thread_file.puts [ video_id, row["date"], row["no"], row["vpos"], comment, row["command"] ].join("\t")
+      command = row["command"]
+      command.gsub!(/\t/, "\\t")
+      command.gsub!(/\n/, "\\n")
+
+      thread_file.puts [ row["date"], row["no"], row["vpos"], comment, command, video_id ].join("\t")
       
     end
     thread_file.close

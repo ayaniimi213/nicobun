96e17ad6216c3a41cf39c8f24eeb9cc435df83b9
Ayahiko Niimi <a-niimi@qc4.so-net.ne.jp>
2013-11-08 22:24:21 +0900

    Update threadjson2tsv.rb

--- threadjson2tsv.rb
+++ threadjson2tsv.rb
@@ -14,10 +14,12 @@ ARGV.each{|filename|
     while line = dat.gets
       
       line = NKF.nkf(opt, line)
-      line.gsub!(/\t/, "\\t")
       row = JSON.parse(line)
-
-      thread_file.puts [ video_id, row["date"], row["no"], row["vpos"], row["comment"], row["command"] ].join("\t")
+      comment = row["comment"]
+      comment.gsub!(/\t/, "\\t")
+      comment.gsub!(/\n/, "\\n")
+      
+      thread_file.puts [ video_id, row["date"], row["no"], row["vpos"], comment, row["command"] ].join("\t")
       
     end
     thread_file.close

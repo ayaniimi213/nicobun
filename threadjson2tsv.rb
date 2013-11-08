f84ece4a6acc494e65149f847452e29983f99df5
Ayahiko Niimi <a-niimi@qc4.so-net.ne.jp>
2013-11-08 12:05:13 +0900

    Update threadjson2tsv.rb

--- threadjson2tsv.rb
+++ threadjson2tsv.rb
@@ -12,14 +12,14 @@ ARGV.each{|filename|
     thread_file = Zlib::GzipWriter.open(thread_dir + "/" + File.basename(filename, ".dat") + "thread.tsv.gz")
     video_id = File.basename(filename, ".dat")
     while line = dat.gets
-
+      
       line = NKF.nkf(opt, line)
       line.gsub!(/\t/, "\\t")
       row = JSON.parse(line)
 
       thread_file.puts [ video_id, row["date"], row["no"], row["vpos"], row["comment"], row["command"] ].join("\t")
-
+      
     end
-  thread_file.close
+    thread_file.close
   end
 }
